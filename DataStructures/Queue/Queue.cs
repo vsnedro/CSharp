07e460dc38edcbbcb344ceaea274972ce0ef8bc0
Vladislav <50212442+vsnedro@users.noreply.github.com>
2020-12-22 04:17:20 +0300

    Data Structures. Queue. Project and class.

---
+++ DataStructures/Queue/Queue.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures.Queue
+{
+    /// <summary>
+    /// Node in a singly linked list
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Node<T>
+    {
+        public T Data { get; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+    }
+
+    /// <summary>
+    /// Queue
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Queue<T> : IEnumerable<T>
+    {
+        #region IEnumerable<T>
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+        #endregion
+    }
+}
