8dee63d5784cee364b8e13ca1c5c4782e5b40bdd
Vladislav <50212442+vsnedro@users.noreply.github.com>
2021-03-12 20:36:08 +0300

    FileTreeBuilder. Добавлен класс строителя дерева файлов FileTreeBuilder.

---
+++ FileTreeBuilder/FileTreeBuilderLib/FileTreeBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTreeBuilderLib
+{
+    public class FileTreeBuilder
+    {
+        public async Task<FileTree> BuildAsync(string fileName)
+        {
+            var parser = new FileTreeParser();
+            var tree = await parser.ParseAsync(fileName);
+
+            var sorter = new FileTreeSorter();
+            sorter.Sort(tree);
+
+            return tree;
+        }
+    }
+}
