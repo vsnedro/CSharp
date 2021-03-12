c89b3137e5ea272cffe7de9d6b976db4b3603a80
Vladislav <50212442+vsnedro@users.noreply.github.com>
2021-03-12 20:33:47 +0300

    FileTreeBuilder. Добавлены классы для дерева файлов FileTree.

---
+++ FileTreeBuilder/FileTreeBuilderLib/FileTree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTreeBuilderLib
+{
+    public class FileTree
+    {
+        public DirectoryNode Root = new("");
+
+        public abstract class TreeNodeCustom
+        {
+            public string Name { get; }
+
+            protected TreeNodeCustom(string name)
+            {
+                Name = name;
+            }
+        }
+
+        public class FileNode : TreeNodeCustom
+        {
+            public int Size { get; }
+
+            public FileNode(string name, int size) : base(name)
+            {
+                Size = size;
+            }
+        }
+
+        public class DirectoryNode : TreeNodeCustom
+        {
+            public List<DirectoryNode> Directories = new();
+            public List<FileNode> Files = new();
+
+            public DirectoryNode(string name) : base(name)
+            {
+            }
+        }
+    }
+}
