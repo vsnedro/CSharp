33ee714d41f3bc6e0b827234e5f8447ec0092d1e
Vladislav <50212442+vsnedro@users.noreply.github.com>
2021-03-12 20:36:38 +0300

    FileTreeBuilder. Добавлен класс для выгрузки дерева файлов FileTreeReporter.

---
+++ FileTreeBuilder/FileTreeBuilderLib/FileTreeReporter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTreeBuilderLib
+{
+    public class FileTreeReporter
+    {
+        public async Task ExportToFileAsync(FileTree tree, string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+
+            await using (var writer = new StreamWriter(fileName))
+            {
+                await ExportToFileInternalAsync(tree.Root, writer);
+            }
+        }
+
+        private async Task ExportToFileInternalAsync(FileTree.DirectoryNode node, TextWriter writer, string indent = "")
+        {
+            // 1. Output directory nodes
+            foreach (var directory in node.Directories)
+            {
+                await writer.WriteLineAsync(indent + $"{directory.Name}\\");
+                // 2. Output recursively all child directory nodes
+                await ExportToFileInternalAsync(directory, writer, indent + "\t");
+            }
+            
+            // 3. Output file nodes
+            foreach (var file in node.Files)
+            {
+                await writer.WriteLineAsync(indent + $"{file.Name} {file.Size}");
+            }
+        }
+    }
+}
