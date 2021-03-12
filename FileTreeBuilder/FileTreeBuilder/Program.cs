285ee1b56b6894ad1b45e4fe6d9ecc574abea4d6
Vladislav <50212442+vsnedro@users.noreply.github.com>
2021-03-12 20:38:18 +0300

    FileTreeBuilder. Добавлен проект для работы с деревом файлов FileTreeBuilder.

---
+++ FileTreeBuilder/FileTreeBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FileTreeBuilderLib;
+
+namespace FileTreeBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var treeBuilder = new FileTreeBuilderLib.FileTreeBuilder();
+            var treeTask = treeBuilder.BuildAsync("D:\\Temp\\files.txt");
+            var reporter = new FileTreeReporter();
+            reporter.ExportToFileAsync(treeTask.Result, "D:\\Temp\\tree.txt");
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
