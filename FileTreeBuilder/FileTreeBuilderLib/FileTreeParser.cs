6a413ce5ab964dd9a09a3a0de771ad16a97f3941
Vladislav <50212442+vsnedro@users.noreply.github.com>
2021-03-12 20:34:47 +0300

    FileTreeBuilder. Добавлен класс парсера для дерева файлов FileTreeParser.

---
+++ FileTreeBuilder/FileTreeBuilderLib/FileTreeParser.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTreeBuilderLib
+{
+    public class FileTreeParser
+    {
+        public async Task<FileTree> ParseAsync(string fileName, bool sort = true)
+        {
+            var lines = await File.ReadAllLinesAsync(fileName);
+
+            return Parse(lines, sort);
+        }
+
+        public FileTree Parse(string[] lines, bool sort = true)
+        {
+            var tree = new FileTree();
+
+            if (sort)
+            {
+                Array.Sort(lines);
+            }
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line)) continue;
+
+                var node = tree.Root;
+                var elems = line.Split('\\', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var elem in elems)
+                {
+                    if (TryGetFileInfo(elem, out string name, out int size))
+                    {
+                        var file = new FileTree.FileNode(name, size);
+                        node.Files.Add(file);
+                    }
+                    else
+                    {
+                        var directory = node.Directories.Find(x => x.Name.Equals(elem));
+                        if (directory == null)
+                        {
+                            directory = new FileTree.DirectoryNode(elem);
+                            node.Directories.Add(directory);
+                        }
+                        node = directory;
+                    }
+                }
+            }
+
+            return tree;
+        }
+
+        private bool TryGetFileInfo(string s, out string name, out int size)
+        {
+            const int fileInfoLength = 2;
+            name = "";
+            size = 0;
+
+            if (s.Contains(' '))
+            {
+                var elems = s.Split(' ');
+                if ((elems.Length == fileInfoLength) && (int.TryParse(elems[1], out size)))
+                {
+                    name = elems[0];
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
